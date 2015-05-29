1137df41a873cb5524302915299a2c999e4d2b86
lukeellison <luke.ellison12@imperial.ac.uk>
2015-05-29 15:53:57 +0100

    I am a genius. Also started adding in a new feature

--- js/data_d3.js
+++ js/data_d3.js
@@ -28,7 +28,7 @@ var chart = d3.select(".chart") // Create the chart space with margins
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var lineGen = d3.svg.line() //function for drawing the line based off built in function
+var lineGen = d3.svg.line().interpolate("linear") //function for drawing the line based off built in function
   .x(function(d) {
     return x(d.time);
   })
@@ -36,69 +36,11 @@ var lineGen = d3.svg.line() //function for drawing the line based off built in f
     return y(d.temperature);
   });
 
-var bisect = d3.bisector(function(d) { return d.time; }).left; // Create custom bisector
+var line = chart.append("path")
+  .attr("class","line")
+  .attr("d", "0"); //Create line element
 
-d3.tsv("data/temp.tsv", type, function(error, data) { // Load data from tsv file
-  x.domain([0, d3.max(data, function(d) { return d.time; })]); //Set domains of scale functions
-  y.domain([0, d3.max(data, function(d) { return d.temperature; })]);
-
-  chart.append("g") // Draw x axis
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-  chart.append("text") // Text label for the x axis
-      .attr("x", width/2 )
-      .attr("y", height + margin.bottom )
-      .style("text-anchor", "middle")
-      .text("Time / s");
-
-  chart.append("g") // Draw y axis
-      .attr("class", "y axis")
-      .call(yAxis);
-
-  chart.append("text") // Text label for the y axis
-      .attr("transform", "rotate(-90)")
-      .attr("y", 0 - margin.left)
-      .attr("x",0 - (height / 2))
-      .attr("dy", "1em")
-      .style("text-anchor", "middle")
-      .text("Temperature / \u00B0Celcius");
-
-  chart.append("path") // Draw line
-    .attr("d", lineGen(data))
-    .attr("class", "line")
-
-  d3.select(".chart") // Mouse stuff
-    .on('mouseover', function() {
-      focus.style('display', null);}) //Display focus when mouse is on chart
-    .on('mouseout', function() {
-      focus.style('display', 'none');}) //Remove is when not
-    .on('mousemove', function() { //Update focus when mouse moves
-      var mouse = d3.mouse(this),
-      x0 = x.invert(mouse[0]),
-      index = bisect(data, x0),
-      d0 = data[index - 1],
-      d1 = data[index];
-      if(d1 == null){
-        d1 = data[data.size()];
-        focus.select("text").attr("x",-9);
-      };
-      var d = x0 - d0.time > d1.time - x0 ? d1 : d0;
-      focus.attr("transform", "translate(" + x(d.time) + "," + y(d.temperature) + ")");
-      focus.select("text").text("(" + d.time + "s, " + d.temperature + "\u00B0C)");
-      focus.select("text").attr("x", function(){
-        var str = focus.attr("transform");
-        str = str.slice(10);
-        var num = parseInt(str);
-        if(num > 800){
-          return -90;
-        }
-        else return 10;
-      })
-  });
-  
-});
+var bisect = d3.bisector(function(d) { return d.time; }).right; // Create custom bisector
 
 var focus = chart.append("g") // Create the focus circle thingy
   .attr("class", "focus")
@@ -110,8 +52,106 @@ focus.append("circle")
 focus.append("text")
   .attr("dy", ".35em"); 
 
-function type(d) { // Function for converting to number from string
-  d.time = +d.time;
-  d.temperature = +d.temperature;
-  return d;
+var mouse = null; //Variable for holding mouse data, updated on mouse movement
+var xOption = d3.select("#xOption").node().value; //Get the axis options from the html
+var yOption = d3.select("#yOption").node().value;
+
+function callback(data){
+      data = extract_payload(data); //Remove id and rev from received data
+
+      x.domain([0, d3.max(data, function(d) { return d.time; })]); //Set domains of scale functions
+      y.domain([0, d3.max(data, function(d) { return d.temperature; })]);
+
+      if (chart.selectAll(".x.axis")[0].length < 1 ){ // if no x axis exists, create one
+        chart.append("g")
+          .attr("class","x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+      } else { // otherwise, update the axis
+        chart.selectAll(".x.axis").transition().duration(1500).call(xAxis);
+      }
+
+      if (chart.selectAll(".y.axis")[0].length < 1 ){ // if no y axis exists, create one
+        chart.append("g")
+          .attr("class","y axis")
+          .call(yAxis);  
+      } else { // otherwise, update the axis
+        chart.selectAll(".y.axis").transition().duration(1500).call(yAxis);
+      }
+      
+      line.transition().duration(1500) // transition from previous paths to new paths
+        .attr("d",lineGen(data));
+
+      d3.select(".chart") // Mouse stuff
+        .on('mouseover', function() {
+          focus.style('display', null);}) //Display focus when mouse is on chart
+        .on('mouseout', function() {
+          focus.style('display', 'none');}) //Remove is when not
+        .on('mousemove', function() { //When mouse moves
+          mouse = d3.mouse(this); //Update mouse position
+          adjustFocus(data, mouse); //Adjust focus
+        });
+      adjustFocus(data, mouse);
+};
+
+getData(); //Initial get of data
+setInterval(getData, 2000); //Re-render every 2 seconds
+
+chart.append("text") // Text label for the x axis
+    .attr("x", width/2 )
+    .attr("y", height + margin.bottom -10 )
+    .style("text-anchor", "middle")
+    .text("Time / s");
+
+chart.append("text") // Text label for the y axis
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x",0 - (height / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .text("Temperature / \u00B0Celcius");
+
+function adjustFocus(data, mouse){ //Adjusts focus based on mouse position
+    var x0 = x.invert(mouse[0]-margin.left),
+    index = bisect(data, x0),
+    d0 = data[index - 1],
+    d1 = data[index];
+    var d = x0 - d0.time > d1.time - x0 ? d1 : d0;
+    focus.attr("transform", "translate(" + x(d.time) + "," + y(d.temperature) + ")");
+    focus.select("text").text("(" + dp1(d.time) + "s, " + dp1(d.temperature) + "\u00B0C)");
+    focus.select("text").attr("x", function(){
+      var str = focus.attr("transform");
+      str = str.slice(10);
+      var num = parseInt(str);
+      if(num > 800){
+        return -80;
+      }
+      else return 10;
+    })
+};
+
+function getData(){ // Function for refreshing the data tag in the html (effectively refreshing the data)
+  if(d3.select(".data")) d3.select("data").remove();
+  d3.select("script").insert("script")
+    .attr("src", "data/example_sensor.js")
+    .attr("class", "data");
+}
+
+function extract_payload(data){   // Extract expected info from json
+  var payload_array = [];
+  data.forEach(function(datum){
+    var payload = datum.payload; // Remove the id and rev from the json
+    payload_array.push(payload); 
+  })
+  return payload_array;
+}
+
+var dp1 = d3.format(".1f");
+
+var units = { // Data of corrisponding units for axis
+  "Time" : "s",
+  "Temperature" : "\u00B0C",
+  "Altitude" : "m",
+  "Pressure" : "atm",
+  "Light" : "cd"
 }
