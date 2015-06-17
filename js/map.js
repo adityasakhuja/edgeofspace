0edc34c594d94860fc7060712c4ceb13f30da3bd
Luke Ellison <luke.ellison12@imperial.ac.uk>
2015-06-17 17:24:26 +0100

    migrating maps to the new database structure

--- js/map.js
+++ js/map.js
@@ -10,14 +10,14 @@ var STARTLAT = 51.4796075,
 var map = null,
     prevFlightPath = null,
     launch_marker = null,
-    prevBalloon_marker = null;
+    balloon_marker = null;
 
 var launch_img = { //Define launching pad image for map
   url: "img/launchpad_tracker_icon_small.png",
   size: new google.maps.Size(57, 17),
   // The origin for this image is 0,0.
   origin: new google.maps.Point(0,0),
-  // The anchor for this image is the base of the flagpole at 0,32.
+  // The anchor for this image is the centre.
   anchor: new google.maps.Point(28, 9)
 };
 
@@ -27,7 +27,7 @@ var balloon_img = { //Define launching balloon image for map
   size: new google.maps.Size(20, 32),
   // The origin for this image is 0,0.
   origin: new google.maps.Point(0,0),
-  // The anchor for this image is the base of the flagpole at 0,32.
+  // The anchor for this image is the base of the balloon at 0,16.
   anchor: new google.maps.Point(0, 16)
 };
 
@@ -45,11 +45,11 @@ function initialize() { // Draw map
 }
 
 function callback(data){ // Draw line when data refreshes
-  var gps_points = return_sorted_gps_coordinates(data)
+  var sorted_data = sort_data(data)
 
   var flightPlanCoordinates = [];
-  gps_points.forEach(function(point){
-    flightPlanCoordinates.push(new google.maps.LatLng(point.lat,point.lng));
+  sorted_data.forEach(function(point){
+    flightPlanCoordinates.push(new google.maps.LatLng(point.LATITUDE,point.LONGITUDE));
   })
 
   flightPath = new google.maps.Polyline({
@@ -63,50 +63,46 @@ function callback(data){ // Draw line when data refreshes
   if(prevFlightPath != null) prevFlightPath.setMap(null);
   prevFlightPath = flightPath;
 
-  console.log(launch_marker);
   if(!launch_marker) //Only draw launch marker once
   launch_marker = new google.maps.Marker({
-      position: new google.maps.LatLng(gps_points[0].lat,gps_points[0].lng),
+      position: new google.maps.LatLng(sorted_data[0].LATITUDE,sorted_data[0].LONGITUDE),
       title:"Launch",
       map: map,
       icon: launch_img
   });
 
+  console.log(balloon_marker);
+  if(!balloon_marker) //Only draw balloon marker first time otherwise update position only
   balloon_marker = new google.maps.Marker({
-      position: new google.maps.LatLng(gps_points[gps_points.length-1].lat,gps_points[gps_points.length-1].lng),
+      position: new google.maps.LatLng(sorted_data[sorted_data.length-1].LATITUDE,sorted_data[sorted_data.length-1].LONGITUDE),
       title:"Current Position",
       map: map,
       icon: "img/balloon_tracker_icon_small.png"
   });
-  if(prevBalloon_marker != null) prevBalloon_marker.setMap(null);
-  prevBalloon_marker = balloon_marker;
+  else balloon_marker.setPosition(new google.maps.LatLng(sorted_data[sorted_data.length-1].LATITUDE,sorted_data[sorted_data.length-1].LONGITUDE));
 }
 
 setInterval(getData, 5000); //Re-render every 2 seconds
 
 function getData(){ // Function for refreshing the data tag in the html (effectively refreshing the data)
   if($(".data").length) $(".data").remove();
+//  var script_el = "<script type='application/javascript' class='data' src='http://balloon.mybluemix.net/getgps'>";
   var script_el = "<script type='application/javascript' class='data' src='data/example_pos.js'>";
   $("script").after(script_el);
 }
 
-function return_sorted_gps_coordinates(data){   // Extract expected info from json and sort it
+function sort_data(data){   // Extract expected info from json and sort it
 
-  var payload_array = [];
-  data.forEach(function(datum){
-    var payload = datum.payload; // Remove the id and rev from the json
-    payload_array.push(payload); 
-  })
-
-  payload_array.sort(function(a, b){ // Sort by time
-    return parseFloat(a.timestamp)-parseFloat(b.timestamp);
+  data.sort(function(a, b){ // Sort by time
+    return timeify(a.PITIME)-timeify(b.PITIME);
   });
 
-  var gps_points = [];
-  payload_array.forEach(function(payload){ // Take gps and put it in expected format
-    var LatLng = {"lat": payload.lat, "lng": payload.lng};
-    gps_points.push(LatLng);
-  })
+  return data;
+}
 
-  return gps_points;
+function timeify(time){
+  var t = time.split(":");
+  var mins = parseInt(t[1]) + parseInt(t[0])*60;
+  var secs = parseInt(t[2]) + mins*60;
+  return secs;
 }
